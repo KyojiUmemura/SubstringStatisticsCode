cd3792cd54cab8570f27dd05184cca5da96d7594
Kyoji Umemura <umekichi@Pro15-2.local>
2017-11-16 18:46:46 +0900

    Try-me.sh now generate files in ../TRY/

--- TRY-ME.sh
+++ TRY-ME.sh
@@ -1,5 +1,7 @@
-./s1-generate-suffix       < CORPUS > CORPUS.s1
-sh ./s2-sort-suffix.sh     < CORPUS.s1 > CORPUS.s2
-./s3-compute-neighbor      < CORPUS.s2 > CORPUS.s3
-./s4-compute-lcp           < CORPUS.s3 > CORPUS.s4
-./s5-detect-class          < CORPUS.s4 > RESULT
+mkdir -p ../TRY
+cp CORPUS ../TRY
+./s1-generate-suffix       < ../TRY/CORPUS > ../TRY/CORPUS.s1
+sh ./s2-sort-suffix.sh     < ../TRY/CORPUS.s1 > ../TRY/CORPUS.s2
+./s3-compute-neighbor      < ../TRY/CORPUS.s2 > ../TRY/CORPUS.s3
+./s4-compute-lcp           < ../TRY/CORPUS.s3 > ../TRY/CORPUS.s4
+./s5-detect-class          < ../TRY/CORPUS.s4 > ../TRY/RESULT
